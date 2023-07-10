c74c3919b6acd299698ecbdb5405985d67a6bf25
Noah Jacinto <noahjacinto@Noahs-MacBook-Air.local>
2023-07-10 15:04:23 -0700

    Revised unit tests

--- python/compress-image/main.py
+++ python/compress-image/main.py
@@ -5,51 +5,62 @@ import requests
 
 
 def krakenio_impl(variables):
-    # Kraken Url for uploading media
-    url = 'https://api.kraken.io/v1/'
-    api_endpoint = url + 'upload'
-
-    # Headers for post request
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36'
-    }
-    # File that we will pass in
-    files = {'file': variables['decoded_image']}
-    # Parameters for post request
-    params = {
-        "auth": {
-            "api_key": variables['api_key'],
-            "api_secret": variables['api_secret_key']
-        },
-        "wait": True,  # Optional: Wait for the optimization to complete
-        "dev": False  # Optional: Set to false to use API
-    }
-    response = requests.post(url=api_endpoint,
-                             headers=headers,
-                             files=files,
-                             data={'data': json.dumps(params)},
-                             timeout=10)
-
-    # Check status code of response
-    if response.status_code == 200:
-        # Request successful, parse the response
-        data = response.json()
-        if data["success"] is True:
-            optimized_url = data["kraked_url"]
-            # Return the contents of the url as decoded
-            return {
-                "success": True,
-                "optimized_image": requests.get(optimized_url).content
-            }
-    else:
-        raise ValueError("KrakenIO failed to compress.")
-
+    try :
+        # Kraken Url for uploading media
+        url = 'https://api.kraken.io/v1/'
+        api_endpoint = url + 'upload'
+    
+        # Headers for post request
+        headers = {
+            'User-Agent':
+            'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36'
+        }
+        # File that we will pass in
+        files = {'file': variables['decoded_image']}
+        # Parameters for post request
+        params = {
+            "auth": {
+                "api_key": variables['api_key'],
+                "api_secret": variables['api_secret_key']
+            },
+            "wait": True,  # Optional: Wait for the optimization to complete
+            "dev": False  # Optional: Set to false to use API
+        }
+        
+        response = requests.post(url=api_endpoint,
+                                 headers=headers,
+                                 files=files,
+                                 data={'data': json.dumps(params)},
+                                 timeout=10)
+    
+        # Check status code of response
+        if response.status_code == 200:
+            # Request successful, parse the response
+            data = response.json()
+            if data["success"] is True:
+                optimized_url = data["kraked_url"]
+                # Return the contents of the url as decoded
+                return {
+                    "success": True,
+                    "optimized_image": requests.get(optimized_url).content
+                }
+    except Exception as kraken_exception:
+        return {
+            "success": False,
+            "message": kraken_exception
+        }
 
 def tinypng_impl(variables):
-    tinify.key = variables['api_key']
-    result_data = tinify.from_buffer(variables['decoded_image']).to_buffer()
+    try:
+        tinify.key = variables['api_key']
+        result_data = tinify.from_buffer(variables['decoded_image']).to_buffer()
+
+    except tinify.errors.AccountError:
+        return {"success": False, "message": "Invalid_API_KEY"}
+    
     return {"success": True, "optimized_image": result_data}
+
+
 
 
 def validate_payload(req):
@@ -92,19 +103,25 @@ def main(req, res):
     try:
         variables = validate_payload(req)
     except Exception as validate_message:
-        return res.json({"success": False, "Invalid Request": str(validate_message)})
+        return res.json({
+            "success": False,
+            "Invalid Request": str(validate_message)
+        })
 
     # Run the api function
-    try:
-        if variables['provider'].lower() == 'krakenio':
-            result = krakenio_impl(variables)
-        else:
-            result = tinypng_impl(variables)
-    except RuntimeError as api_message:
-        return res.json({"success": False, "API Failed to compress image": api_message})
+    if variables['provider'].lower() == 'krakenio':
+        result = krakenio_impl(variables)
+    else:
+        result = tinypng_impl(variables)
 
     # Return a response in JSON
-    return res.json({
-        "success:": True,
-        "image": str(base64.b64encode(result['optimized_image']))
-    })
+    if result['success']:
+        return res.json({
+            "success:": True,
+            "image": str(base64.b64encode(result['optimized_image']))
+        })
+    else:
+        return res.json({
+            "success": False,
+            "message": result['message']
+        })
--- python/compress-image/test_main.py
+++ python/compress-image/test_main.py
@@ -5,6 +5,7 @@ import main
 
 
 class TestMain(unittest.TestCase):
+
     def setUp(self):
         # self.api_key = "YOUR_API_KEY"
         # self.encoded_image = b"YOUR_ENCODED_IMAGE"
@@ -15,47 +16,105 @@ class TestMain(unittest.TestCase):
         self.api_key_krakenio = "f66cec6f44df73d3ba48d8dbce302738"
         self.secret_api_key_krakenio = "7bbd29dd53c00a068b7ebe20b074540a0d7cea9d"
 
+
+        # Result data for 0b
+        self.result_data_empty = b""
+        
+        # Decoded original image file 1kb
         image_file_1kb = open("python/compress-image/test/1kb/1kb.png", "rb")
         self.decoded_image_1kb = image_file_1kb.read()
 
-        encoded_result_1kb = open("python/compress-image/test/1kb/1kb_result_encoded", "r")
-        self.result_data_1kb = base64.b64decode(encoded_result_1kb.read())
+        # Result data for 1kb tinypng
+        encoded_result_1kb_tinypng = open("python/compress-image/test/1kb/1kb_result_encoded_tinypng.txt", "r")
+        self.result_data_1kb_tinypng = base64.b64decode(encoded_result_1kb_tinypng.read())
 
-        # Gathering variables for 3MB image
+        # Result data for 1kb krakenio
+        encoded_result_1kb_krakenio = open("python/compress-image/test/1kb/1kb_result_encoded_krakenio.txt", "r")
+        self.result_data_1kb_krakenio = base64.b64decode(encoded_result_1kb_krakenio.read())
+        
+        # Decoded original image file 3mb
         image_file_3mb = open("python/compress-image/test/3mb/3mb.jpg", "rb")
         self.decoded_image_3mb = image_file_3mb.read()
 
-        encoded_result_3mb = open("python/compress-image/test/3mb/3mb_result_encoded", "r")
-        self.result_data_3mb = base64.b64decode(encoded_result_3mb.read())
+        # Result data for 3mb tinypng
+        encoded_result_3mb_tinypng = open("python/compress-image/test/3mb/3mb_result_encoded_tinypng.txt", "r")
+        self.result_data_3mb_tinypng = base64.b64decode(encoded_result_3mb_tinypng.read())
+
+        # Result data for 3mb krakenio
+        encoded_result_3mb_krakenio = open("python/compress-image/test/3mb/3mb_result_encoded_krakenio.txt", "r")
+        self.result_data_3mb_krakenio = base64.b64decode(encoded_result_3mb_krakenio.read())
 
         self.variables_tinypng = [{
-            "api_key": self.api_key_tinypng,
-            "decoded_image": self.decoded_image_1kb
+                #1 kb image
+                "api_key": self.api_key_tinypng,
+                "decoded_image": self.decoded_image_1kb,
             },
 
-            {
-            "api_key": self.api_key_tinypng,
-            "decoded_image": self.decoded_image_3mb
+            {   
+                #3kb image
+                "api_key": self.api_key_tinypng,
+                "decoded_image": self.decoded_image_3mb,
+            },
+
+            { #Wrong api key
+                "api_key": "R4nM3B54NbHNcHblC0XXl0LZyVBgZ",
+                "decoded_image": self.decoded_image_1kb,
+            },
+
+            { #If a person submits empty image
+                "api_key": self.api_key_tinypng,
+                "decoded_image": b"",
             }
         ]
 
         self.variables_krakenio = [{
+            #1kb image
             "api_key": self.api_key_krakenio,
-            "secret_api_key": self.secret_api_key_krakenio,
+            "api_secret_key": self.secret_api_key_krakenio,
             "decoded_image": self.decoded_image_1kb
             },
             
             {
             "api_key": self.api_key_krakenio,
-            "secret_api_key": self.secret_api_key_krakenio,
+            "api_secret_key": self.secret_api_key_krakenio,
             "decoded_image": self.decoded_image_3mb
-            }
-        ]
+            },
 
-    def test_tinypng_1kb(self):
-        # Output Validation 1KB
-        for image in self.variables_tinypng:
-            print(image)
+            {
+            "api_key": self.api_key_krakenio,
+            "api_secret_key": self.secret_api_key_krakenio,
+            "decoded_image": self.decoded_image_3mb
+            },
+                                   
+            {
+            "api_key": self.api_key_krakenio,
+            "api_secret_key": self.secret_api_key_krakenio,
+            "decoded_image": self.decoded_image_3mb
+            },
+                                   
+        ]
+        return None
+
+    def test_tinypng(self):
+        # Output validation 1KB
+        result = main.tinypng_impl(self.variables_tinypng[0])
+        self.assertTrue(result["success"])
+        self.assertEqual(result["optimized_image"], self.result_data_1kb_tinypng)
+
+        # Output validation 3MB
+        result = main.tinypng_impl(self.variables_tinypng[1])
+        self.assertTrue(result["success"])
+        self.assertEqual(result["optimized_image"], self.result_data_3mb_tinypng)
+
+        # If user inputs wrong credentials
+        result = main.tinypng_impl(self.variables_tinypng[2])
+        print(result)
+        self.assertFalse(result["success"])
+        self.assertEqual(result["message"], "Invalid_API_KEY")
+
+    def test_krakenio(self):
+        # Output validation 1KB
+        result = main.krakenio_impl(self.variables_krakenio[1])
 
 if __name__ == '__main__':
     unittest.main()
