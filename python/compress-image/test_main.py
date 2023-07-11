a4c5b9672436434647f5cc9118392a4fa9400474
rubynguyen1510 <50710444+rubynguyen1510@users.noreply.github.com>
2023-07-11 22:03:04 +0000

    added unit test for krakenio

--- python/compress-image/test_main.py
+++ python/compress-image/test_main.py
@@ -3,25 +3,24 @@ import base64
 import pathlib
 import secret
 import main
-import secret
+import tinify
+import requests
 # from main import tinypng_impl
-
 
 class TestMain(unittest.TestCase):
 
-    def setUp(self):
-        # self.api_key_tinypng = YOUR API_KEY
-        # self.api_key_krakenio = secret.API_KEY_KRAKENIO
-        # self.secret_api_key_krakenio = secret.SECRET_API_KEY_KRAKENIO
-
-        self.api_key_tinypng = secret.API_KEY_TINYPNG
-        self.api_key_krakenio = secret.API_KEY_KRAKENIO
-        self.secret_api_key_krakenio = secret.SECRET_API_KEY_KRAKENIO
-
-    def test_tinypng_small(self):
+    def test_krakenio_small(self):
         # Output validation 1KB
-        want = base64.b64decode( pathlib.Path("python/compress-image/test/1kb/1kb_result_encoded_tinypng.txt").read_text() )
-        got = main.tinypng_impl({"api_key": self.api_key_tinypng, "decoded_image": pathlib.Path("python/compress-image/test/1kb/1kb.png").read_bytes()})
+        want = base64.b64decode(pathlib.Path(
+            secret.RESULT_1KB_KRAKENIO).read_text())
+        got = main.krakenio_impl({"api_key": secret.API_KEY_KRAKENIO, "api_secret_key": secret.SECRET_API_KEY_KRAKENIO, "decoded_image": pathlib.Path(secret.IMAGE_1KB).read_bytes()})
+        self.assertIsNone(got[0])
+        self.assertEqual(got[1], want)
+
+    def test_krakenio_big(self):
+        # Output validation 3MB
+        want = base64.b64decode(pathlib.Path(secret.RESULT_3MB_KRAKENIO).read_text())
+        got = main.krakenio_impl({"api_key": secret.API_KEY_KRAKENIO, "api_secret_key": secret.SECRET_API_KEY_KRAKENIO, "decoded_image": pathlib.Path(secret.IMAGE_3MB).read_bytes()})
         self.assertIsNone(got[0])
         self.assertEqual(got[1], want)
 
