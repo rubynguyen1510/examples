4600b973dd99c60b8c359d6e80cefbb6db757a12
Noah Jacinto <noahjacinto@Noahs-MacBook-Air.local>
2023-07-17 12:01:06 -0700

    removed secret.py in gitignore and set None value to secret keys

---
+++ python/compress-image/secret.py
@@ -0,0 +1,12 @@
+'''
+API Key for tinyPNG and KrakenIO are stored here
+
+You should be in python/compress-image directory to run test_main.py
+'''
+API_KEY_TINYPNG = None
+API_KEY_KRAKENIO = None
+SECRET_API_KEY_KRAKENIO = None
+
+RESULT_1KB_TINYPNG = "test/1kb_result_encoded_tinypng.txt"
+RESULT_1KB_KRAKENIO = "test/1kb_result_encoded_krakenio.txt"
+IMAGE_1KB = "test/1kb.png"
