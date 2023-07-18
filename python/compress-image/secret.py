54904c3bb9b191651e513bf1ddd1caf277ea6fe9
Noah Jacinto <noahjacinto@Noahs-MacBook-Air.local>
2023-07-17 17:37:41 -0700

    Removed secret.py from gitignore

--- python/compress-image/secret.py
+++ python/compress-image/secret.py
@@ -3,6 +3,6 @@ API Key for tinyPNG and KrakenIO are stored here
 
 You should be in python/compress-image directory to run test_main.py
 '''
-API_KEY_TINYPNG = "R4nM3B54NbHNcHblC0XXl0LZyV82PBgZ"
-API_KEY_KRAKENIO = "9418b665c7525f940aa0433568283081"
-SECRET_API_KEY_KRAKENIO = "28c98b9484af1179d5ff15964cbb8e62004ebb22"
+API_KEY_TINYPNG = None
+API_KEY_KRAKENIO = None
+SECRET_API_KEY_KRAKENIO = None
